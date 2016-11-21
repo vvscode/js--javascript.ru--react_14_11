efcb4baea29b01761b7b66b29f4ae43053616bea
Vasiliy Vanchuk <v.vanchuk@tut.by>
2016-11-21 13:20:40 +0300

    Update CommentList.js

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -30,7 +30,6 @@ class CommentList extends Component {
 
     render() {
       return (<div className="comment-list">
-        {/*Вот тут ты перемудрил, не советую так писать. В остальном все ок*/}
         {this.state.isOpen ? this.renderOpened() : this.renderClosed()}
       </div>)
     }

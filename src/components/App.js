47e307d7afe1017f8b9f343e42cae83f2d97ae44
Vasiliy Vanchuk @vvscode <v.vanchuk@tut.by>
2016-11-27 17:23:57 +0300

    Add DateRange component

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,11 @@ import React, { Component, PropTypes } from 'react'
 import ArticleList from './ArticleList'
 import Select from 'react-select'
 import Chart from './Chart'
+import DateRange from './DateRange'
+
 import 'react-select/dist/react-select.css'
+
+const today = new Date();
 
 class App extends Component {
     static propTypes = {
@@ -10,16 +14,23 @@ class App extends Component {
     };
 
     state = {
-        selected: null
+        selected: null,
+        from: today,
+        to: today,
     }
+
+    onSelectDate = ({from, to}) => this.setState({from , to})
 
     render() {
         const options = this.props.articles.map(article => ({
             label: article.title,
             value: article.id
         }))
+        const { from, to } = this.state
         return (
             <div>
+                { (from && to && +from !== +to) ? `${from} - ${to}` : (from || to) ? `${from||to}` : ''}
+                <DateRange from={from} to={to} onSelectDate={this.onSelectDate} />
                 <Chart />
                 <ArticleList articles={this.props.articles} />
                 <Select options = {options} value = {this.state.selected} onChange = {this.handleChange} multi = {true}/>
---
+++ src/components/DateRange.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import moment from 'moment';
+import DayPicker, { DateUtils } from 'react-day-picker';
+
+import 'react-day-picker/lib/style.css';
+
+export default class Range extends Component {
+  handleDayClick = (e, day) => {
+    const range = DateUtils.addDayToRange(day, this.props);
+    this.props.onSelectDate(range);
+  }
+  render() {
+    const { from, to } = this.props;
+    return (
+        <DayPicker
+          numberOfMonths={ 1 }
+          selectedDays={ day => DateUtils.isDayInRange(day, { from, to }) }
+          onDayClick={ this.handleDayClick }
+          className="DateRange"
+        />
+    );
+  }
+}
